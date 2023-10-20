6139a6a8ddba31f24d27d5161450674d20bad673
Roland <roland@siga.pc>
2023-10-20 21:25:43 +0200

    routing works

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {RouterModule, Routes} from "@angular/router";
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -13,15 +14,35 @@ import {MatMenuModule} from "@angular/material/menu";
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
 import { BlogsComponent } from './blogs/blogs.component';
+import { RatingsComponent } from './ratings/ratings.component';
+
+
+const myRoutes: Routes =[
+  {
+    path:'',
+    component:HomeComponent
+  },
+  {
+    path:'blogs',
+    component:BlogsComponent
+  },
+  {
+    path:'ratings',
+    component:RatingsComponent
+  }
+
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     HomeComponent,
-    BlogsComponent
+    BlogsComponent,
+    RatingsComponent
   ],
   imports: [
+    RouterModule.forRoot(myRoutes),
     BrowserModule,
     BrowserAnimationsModule,
     // lbarModule,
