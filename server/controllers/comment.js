ea7d34f5e7be6bf21172b0c575342850c1adb403
Marko <Github@Salig.net>
2023-11-07 11:34:52 +0100

    make all comment request return a join with the author

--- server/controllers/comment.js
+++ server/controllers/comment.js
@@ -1,10 +1,13 @@
 const Joi = require('joi-oid')
 const Comment = require('../models/comment');
 const BlogEntry = require("../models/blogentry");
+const User = require("../models/user");
+const mongoose = require("mongoose");
 
 const commentSchema = Joi.object({
   authorId: Joi.objectId().required(),
   blogEntryId: Joi.objectId().required(),
+  title: Joi.string().required(),
   text: Joi.string().required(),
   review: Joi.number().min(0).max(5)
 });
@@ -23,11 +26,37 @@ async function readAll() {
 }
 
 async function read(id) {
-  return Comment.findById(id);
+  return Comment.aggregate([{
+    $match: {_id: new mongoose.Types.ObjectId(id)}
+  }, {
+    $lookup: {
+      from: "users", localField: "authorId", foreignField: "_id", as: "authors"
+    }
+  }, {
+    $addFields: {
+      author: { $arrayElemAt: ["$authors", 0] }    }
+  }, {
+    $project: {
+      "author.hashedPassword": 0, "author.__v": 0, "author.email": 0, authors: 0, __v: 0
+    }
+  }]).exec();
 }
 
 async function getByBlogEntryId(id) {
-  return Comment.find({blogEntryId: id});
+  return Comment.aggregate([{
+    $match: {blogEntryId: new mongoose.Types.ObjectId(id)}
+  }, {
+    $lookup: {
+      from: "users", localField: "authorId", foreignField: "_id", as: "authors"
+    }
+  }, {
+    $addFields: {
+      author: { $arrayElemAt: ["$authors", 0] }    }
+  }, {
+    $project: {
+      "author.hashedPassword": 0, "author.__v": 0, authors: 0, __v: 0
+    }
+  }]).exec();
 }
 
 async function update(commentId, comment) {
